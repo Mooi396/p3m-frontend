26e2fb18631b017f05ec108bdcf8055aa5b046b7
Mooi396 <Hilmikhalim030906@gmail.com>
2026-03-06 05:56:49 +0700

    first commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,199 @@
+import React from "react";
+import {
+  Navbar,
+  MobileNav,
+  Typography,
+  Button,
+  Menu,
+  MenuHandler,
+  MenuList,
+  MenuItem,
+  Avatar,
+  Card,
+  IconButton,
+} from "@material-tailwind/react";
+import {
+  CubeTransparentIcon,
+  UserCircleIcon,
+  CodeBracketSquareIcon,
+  Square3Stack3DIcon,
+  ChevronDownIcon,
+  Cog6ToothIcon,
+  InboxArrowDownIcon,
+  LifebuoyIcon,
+  PowerIcon,
+  RocketLaunchIcon,
+  Bars2Icon,
+  NewspaperIcon,
+  CalendarDateRangeIcon,
+  DocumentTextIcon,
+} from "@heroicons/react/24/solid";
+
+// nav list menu
+function ProfilMenu() {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+  const closeMenu = () => setIsMenuOpen(false);
+
+  return (
+    <Menu open={isMenuOpen} handler={setIsMenuOpen} placement="bottom-start">
+      <MenuHandler>
+        <Button
+          variant="text"
+          color="black"
+          className="flex items-center gap-1 py-1.5 px-2"
+        >
+          <CalendarDateRangeIcon className="h-[18px] w-[18px]" />
+          Profil
+          <ChevronDownIcon
+            strokeWidth={2.5}
+            className={`h-3 w-3 transition-transform ${
+              isMenuOpen ? "rotate-180" : ""
+            }`}
+          />
+        </Button>
+      </MenuHandler>
+
+      <MenuList>
+        <MenuItem onClick={closeMenu}>Agenda Nasional</MenuItem>
+        <MenuItem onClick={closeMenu}>Agenda Wilayah</MenuItem>
+        <MenuItem onClick={closeMenu}>Agenda Kampus</MenuItem>
+      </MenuList>
+    </Menu>
+  );
+}
+
+function AgendaMenu() {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+  const closeMenu = () => setIsMenuOpen(false);
+
+  return (
+    <Menu open={isMenuOpen} handler={setIsMenuOpen} placement="bottom-start">
+      <MenuHandler>
+        <Button
+          variant="text"
+          color="black"
+          className="flex items-center gap-1 py-1.5 px-2"
+        >
+          <CalendarDateRangeIcon className="h-[18px] w-[18px]" />
+          Agenda
+          <ChevronDownIcon
+            strokeWidth={2.5}
+            className={`h-3 w-3 transition-transform ${
+              isMenuOpen ? "rotate-180" : ""
+            }`}
+          />
+        </Button>
+      </MenuHandler>
+
+      <MenuList>
+        <MenuItem onClick={closeMenu}>Agenda Nasional</MenuItem>
+        <MenuItem onClick={closeMenu}>Agenda Wilayah</MenuItem>
+        <MenuItem onClick={closeMenu}>Agenda Kampus</MenuItem>
+      </MenuList>
+    </Menu>
+  );
+}
+
+function LaporanMenu() {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+  const closeMenu = () => setIsMenuOpen(false);
+
+  return (
+    <Menu open={isMenuOpen} handler={setIsMenuOpen} placement="bottom-start">
+      <MenuHandler>
+        <Button
+          variant="text"
+          color="black"
+          className="flex items-center gap-1 py-1.5 px-2"
+        >
+          <DocumentTextIcon className="h-[18px] w-[18px]" />
+          Laporan
+          <ChevronDownIcon
+            strokeWidth={2.5}
+            className={`h-3 w-3 transition-transform ${
+              isMenuOpen ? "rotate-180" : ""
+            }`}
+          />
+        </Button>
+      </MenuHandler>
+
+      <MenuList>
+        <MenuItem onClick={closeMenu}>Laporan Tahunan</MenuItem>
+        <MenuItem onClick={closeMenu}>Laporan Keuangan</MenuItem>
+        <MenuItem onClick={closeMenu}>Laporan Kegiatan</MenuItem>
+      </MenuList>
+    </Menu>
+  );
+}
+
+function NavList() {
+  return (
+    <div className="flex items-center gap-4">
+      <ProfilMenu />
+
+      {/* BERITA */}
+      <Button variant="text" color="Black" className="flex items-center gap-2">
+        <NewspaperIcon className="h-[18px] w-[18px]" />
+        Berita
+      </Button>
+
+      {/* AGENDA */}
+      <AgendaMenu />
+
+      {/* LAPORAN */}
+      <LaporanMenu />
+    </div>
+  );
+}
+
+export function ComplexNavbar() {
+  const [isNavOpen, setIsNavOpen] = React.useState(false);
+
+  const toggleIsNavOpen = () => setIsNavOpen((cur) => !cur);
+
+  React.useEffect(() => {
+    window.addEventListener(
+      "resize",
+      () => window.innerWidth >= 960 && setIsNavOpen(false),
+    );
+  }, []);
+
+  return (
+    <Navbar className="mx-auto max-w-screen-3xl p-2 lg:pl-6">
+      <div className="relative mx-auto flex items-center justify-between">
+        <Typography
+          as="a"
+          href="#"
+          className="mr-4 ml-2 cursor-pointer py-1.5 font-medium"
+        >
+          <div className="text-black font-bold leading-tight">
+            <div className="text-lg">Forum Kepala P3M</div>
+            <div className="text-lg">POLITEKNIK SE-INDONESIA</div>
+          </div>
+        </Typography>
+        <div className="hidden lg:block">
+          <NavList />
+        </div>
+        <IconButton
+          size="sm"
+          color="blue-gray"
+          variant="text"
+          onClick={toggleIsNavOpen}
+          className="ml-auto mr-2 lg:hidden"
+        >
+          <Bars2Icon className="h-6 w-6" />
+        </IconButton>
+
+        <Button size="sm" className="bg-black text-white hover:bg-gray-900">
+          <span>Log In</span>
+        </Button>
+      </div>
+      <MobileNav open={isNavOpen} className="overflow-scroll">
+        <NavList />
+      </MobileNav>
+    </Navbar>
+  );
+}
---
+++ src/pages/home.jsx
@@ -0,0 +1,11 @@
+import { ComplexNavbar } from "../components/navbar";
+
+function Home() {
+  return (
+    <>
+      <ComplexNavbar />
+    </>
+  );
+}
+
+export default Home;
